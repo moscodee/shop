93166a1bb6bb54a7a6fec945cd2d046d2407f0e3
moscode <moscodeprog@gmail.com>
2024-07-29 09:34:41 +0330

    fetchApi fixed

--- src/components/FetchApi.jsx
+++ src/components/FetchApi.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const FetchApi = async (url, path) => {
-  return axios(`${url}${path}`).then((res) => {
+  return axios.get(`${url}${path}`).then((res) => {
     return res;
   });
 };
